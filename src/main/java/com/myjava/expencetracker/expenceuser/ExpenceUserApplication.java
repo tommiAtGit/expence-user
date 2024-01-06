f8ef49de642aba6a83be285d150da041d47ba431
Tommi <tommi.korteikko@iCloud.com>
2024-01-06 22:24:31 +0200

    inital commit

---
+++ src/main/java/com/myjava/expencetracker/expenceuser/ExpenceUserApplication.java
@@ -0,0 +1,13 @@
+package com.myjava.expencetracker.expenceuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenceUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenceUserApplication.class, args);
+	}
+
+}
